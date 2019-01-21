081687fb9ebe1bcc1cd64518975686255e0c08e4
Lopos Florin <loposflorin@gmail.com>
2019-01-21 21:42:23 +0200

    code refactoring / minor fixes

--- js/app.js
+++ js/app.js
@@ -1,31 +1,62 @@
 const cards = document.querySelectorAll ('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
-  this.classList.toggle('flip');
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-    if(!hasFlippedCard) {
-      //first click
-      hasFlippedCard = true;
-      firstCard = this;
-    } else {
-      //second click
-      hasFlippedCard = false;
-      secondCard = this;
-      // check if the cards match
-      if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        // if the cards match
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-      }else{
-        //if cards dont match, set a delay before flipping back
-        setTimeout(() => {                              firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-      },1500);
-    }
+  this.classList.add('flip');
+
+  if(!hasFlippedCard) {
+    //first click
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
   }
+    //second click
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
 };
+
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+    isMatch ? disableCards() : unflipCards();
+};
+
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
+
+  resetBoard();
+};
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+  firstCard.classList.remove('flip');
+  secondCard.classList.remove('flip');
+
+  resetBoard();
+  },1500);
+};
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
+}
+
+/*
+shuffle function needs to be added
+game has the same layout everytime
+*/
 
 cards.forEach(card => card.addEventListener('click', flipCard));
